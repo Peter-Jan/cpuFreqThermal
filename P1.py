69558d553b759c0745ac57a3a0ae17fbeb76b108
Peter Jan <pjan89@gmail.com>
2018-02-21 17:32:54 -0500

    data_logging

--- P1.py
+++ P1.py
@@ -22,18 +22,19 @@ file = open(P0_cluster_freq_set,"w")
 file.write(freq)
 file.close()
 
-freq = "".join(sys.argv[2])
+freq1 = "".join(sys.argv[2])
 file = open(P4_cluster_freq_set,"w")
-file.write(freq)
+file.write(freq1)
 file.close
 
-#run testbench
-#os.system('./TPBench.exe')
+dur = sys.argv[3]
 
+#start timer
 psOut = psutil.cpu_times()
 ps0 = psOut[0]
 ps1 = ps0
-		
+
+
 file1 = open(C4_thermal_sensor,"r")
 file2 = open(C5_thermal_sensor,"r")
 file3 = open(C6_thermal_sensor,"r")
@@ -43,9 +44,14 @@ file6 = open("data_log/C5_temp_"+freq+".txt","a")
 file7 = open("data_log/C6_temp_"+freq+".txt","a")
 file8 = open("data_log/C7_temp_"+freq+".txt","a")
 file9 = open("data_log/Power_"+freq+".txt","a")
+file10 = open(P0_cluster_freq_read,"r")
+file11 = open(little_micro_volts,"r")
+file12 = open(P4_cluster_freq_read,"r")
+file13 = open(big_micro_volts,"r")
 
-#test read
-while(ps1-ps0<120):
+
+#Record Data
+while(ps1-ps0<duration):
 	psOut = psutil.cpu_times()
 	pstemp = psOut[0]
 
@@ -58,32 +64,34 @@ while(ps1-ps0<120):
 		C4temp = file1.read()	 
 		C5temp = file2.read()	 
 		C6temp = file3.read()	 
-		C7temp = file4.read()	 
+		C7temp = file4.read()
+		P0freq = file10.read()
+		P0volt = file11.read()
+		P4freq = file12.read()
+		P4volt = file13.read()	 
 		print "Temp C4: ", C4temp
 		print "Temp C5: ", C5temp
 		print "Temp C6: ", C6temp
 		print "Temp C7: ", C7temp
 		print "Power: ", power
+		print "P0_Cluster_freq: ", P0freq
+		print "P0_Cluster_volt: ", P0volt
+		print "P4_cluster_freq: ", P4freq
+		print "P4_cluster volt: ", P4volt
+		print "Time: ",psOut,"\n"
 
 		file5.write(C4temp.replace('\n','')+",")
 		file6.write(C5temp.replace('\n','')+",")
 		file7.write(C6temp.replace('\n','')+",")
 		file8.write(C7temp.replace('\n','')+",")
-		file9.write(power+",")		 
+		file9.write(power+",")
+		file10.write(P0freq.replace('\n','')+",")
+		file11.write(P0volt.replace('\n','')+",")
+		file12.write(P4freq.replace('\n','')+",")
+		file13.write(P0volt.replace('\n','')+",")
 
-		file = open(P0_cluster_freq_read,"r")
-		print "P0_Cluster_freq: ",file.read()
-		file.close
-		file = open(little_micro_volts,"r")
-		print "P0_Cluster_volt: ",file.read()
-		file.close
-		file = open(P4_cluster_freq_read,"r")
-		print "P4_cluster_freq: ",file.read()
-		file.close
-		file = open(big_micro_volts,"r")
-		print "P4_cluster volt: ",file.read()
-		file.close
-		print "Time: ",psOut,"\n"
+
+
 file1.close
 file2.close
 file3.close
@@ -93,5 +101,9 @@ file6.close
 file7.close
 file8.close
 file9.close
+file10.close
+file11.close
+file12.close
+file13.close
 
 
