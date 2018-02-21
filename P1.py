b475c207d316f7e4b77ca142fe3446a10b4d9626
Peter-Jan <pjan89@gmail.com>
2018-02-21 18:13:02 -0500

    freq/volt

--- P1.py
+++ P1.py
@@ -17,12 +17,12 @@ file = open(P4_cluster_gov,"w")
 file.write("userspace")
 file.close
 
-freq = "".join(sys.argv[1])
+freq1 = "".join(sys.argv[1])
 file = open(P0_cluster_freq_set,"w")
 file.write(freq)
 file.close()
 
-freq1 = "".join(sys.argv[2])
+freq = "".join(sys.argv[2])
 file = open(P4_cluster_freq_set,"w")
 file.write(freq1)
 file.close
