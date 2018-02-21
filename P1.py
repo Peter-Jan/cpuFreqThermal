9b4973f2808adc8def8ee530f11d3fa769012f1b
Peter-Jan <pjan89@gmail.com>
2018-02-21 02:29:30 -0500

    testbench,psutil,and telnet

--- P1.py
+++ P1.py
@@ -1,8 +1,22 @@
 import os
 from sysfs_paths import *
+import psutil
+import telnetlib
 
-name = C0_cpu_freq_set
-file1 = open(name,"w")
-print file1.write(200000)
+#connect to telenet
+tn = telnetlib.Telnet('192.168.4.1')
+
+#run testbench
+os.system('./TPBench.exe')
+
+#test read
+print tn.read_some()
+name = C0_cpu_freq_read
+file1 = open(name,"r")
+print file1.read()
 file1.close
 
+#test write
+file1 = open(C0_cpu_freq_set,"w")
+#file1.write("200000")
+file1.close()
