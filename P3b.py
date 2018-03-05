631210e7c56151b05677c2f508063d0c2c8db402
Peter Jan <pjan89@gmail.com>
2018-03-04 19:07:22 -0500

    cpu_usage

--- P3b.py
+++ P3b.py
@@ -6,10 +6,10 @@ import sys
 import string
 import time
 
-#connect to telnet
+#connect to telenet
 tn = telnetlib.Telnet('192.168.4.1')
 
-#set frequency of clusters
+#set frequency of cluster
 file = open(P0_cluster_gov,"w")
 file.write("userspace")
 file.close
@@ -18,5 +18,35 @@ file = open(P4_cluster_gov,"w")
 file.write("userspace")
 file.close
 
-freq1 = "".join(sys.argv[1])
-file = open(P0
+# #start timer
+# psOut = psutil.cpu_times()
+# ps0 = psOut[0]
+# ps1 = ps0
+
+
+#Record Data
+while(1):
+
+	usage = psutil.cpu_percent(interval=None)
+	print usage
+	# ps1 = pstemp
+	# powerOut =  tn.read_until("\n")
+	# power = powerOut[12:17]
+	# psOut = psOut[0]
+	# file1 = open(C4_thermal_sensor,"r")
+	# file2 = open(C5_thermal_sensor,"r")
+	# file3 = open(C6_thermal_sensor,"r")
+	# file4 = open(C7_thermal_sensor,"r")
+
+	# C4temp = file1.read()
+	# C5temp = file2.read()
+	# C6temp = file3.read()
+	# C7temp = file4.read()
+
+	# file1.close
+	# file2.close
+	# file3.close
+	# file4.close
+
+
+
