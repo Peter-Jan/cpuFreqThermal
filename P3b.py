dc6ff05f43d1e837dc1c73e03bac663680f9a1c7
Peter-Jan <pjan89@gmail.com>
2018-03-04 22:09:45 -0500

    controller completed

--- P3b.py
+++ P3b.py
@@ -5,11 +5,16 @@ import telnetlib
 import sys
 import string
 import time
+import math
+
+#set controller gains
+P = 1
+I = 1
 
 #connect to telenet
 tn = telnetlib.Telnet('192.168.4.1')
 
-#set frequency of cluster
+#set governor to userspace
 file = open(P0_cluster_gov,"w")
 file.write("userspace")
 file.close
@@ -24,7 +29,15 @@ USAGE_THRESHOLD = 80
 FREQ_AVAIL = [200000, 300000, 400000, 500000, 600000, 700000, 800000, 900000,
 	      1000000, 1100000, 1200000, 1300000, 1400000, 1500000, 1600000, 1700000,
 	      1800000, 1900000, 2000000]
-#Record Data
+FREQ_RES = 19
+TARGET_LOAD = USAGE_THRESHOLD * 0.8
+
+#initialize
+headroom = 0
+headroom_integral = 0
+i = 10
+curFreq = FREQ_AVAIL[i]
+max_allowed = FREQ_AVAIL[i]
 
 while(1):
 
@@ -63,24 +76,33 @@ while(1):
 	maxUsage = max([C4_usage,C5_usage,C6_usage,C7_usage])
 	maxTemp = max([C4temp,C5temp,C6temp,C7temp])
 	headroom = TEMP_LIM-maxTemp
-
-# 	if headroom <= 0:
-# #		file = open(P4_cluster_freq_set,"w")
-# #		file.write(freq)
-# #		file.close
-# #		headroom_integral = 0
-# 	else:
-# #		steps = floor(headroom*P+headroom_integral*I)
-# #		max_allowed += steps
-
-# 	if maxUsage > USAGE_THRESHOLD:
-# #		file = open(P4_cluster_freq_set,"w")
-# #		file.write(max_allowed)
-# #		file.close
-# 	else:
-#		file = open(P4_cluster_freq_set,"w")
-#		file.write(freq)
-#		file.close
-
+	curUsage = maxUsage
+ 	if headroom <= 0:
+		i-=1
+		max_allowed = FREQ_AVAIL[i];
+		headroom_integral = 0
+ 	else:
+		steps = math.floor(headroom*P+headroom_integral*I)
+		i += int(steps)
+		if i>18:
+			i = 18
+		max_allowed = FREQ_AVAIL[i]
+		headroom_integral += headroom
+ 	if maxUsage > USAGE_THRESHOLD:
+		file = open(P4_cluster_freq_set,"w")
+		file.write(str(max_allowed))
+		file.close
+		curFreq = max_allowed
+	else:
+		if curUsage > 0:
+			newFreq = round((curFreq/(TARGET_LOAD/curUsage))/100000)*100000
+		else:
+			newFreq = FREQ_AVAIL[0]
+		file = open(P4_cluster_freq_set,"w")
+		file.write(str(newFreq))
+		file.close
+		curFreq = newFreq
 	print "maxTemp: ",maxTemp
-	print "test: ", FREQ_AVAIL[3]
+	print "steps: ",steps
+	print "newFreq: ",newFreq
+	print "curFreq: ", curFreq, "\n"
